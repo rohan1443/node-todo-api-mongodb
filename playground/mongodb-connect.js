23a136109c2c4ddc8cfa939c1c32e560f17f73a2
Rohan Mazumdar <rohan1443@gmail.com>
2017-06-20 00:02:24 +0530

    adding a new collection Users

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,18 +7,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to the MongoDB Server')
   }
   console.log('connected to the MongoDB server')
-  
-  db.collection('Todos').insertOne({
-    text: 'something to do',
-    completed: false
-  }, (err, result) => {
-    if(err){
-      return console.log('Unable to insert Todo', err)
-    }
 
+  // db.collection('Todos').insertOne({
+  //   text: 'something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable to insert Todo', err)
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
+
+  db.collection('Users').insertOne({
+    name: 'Rohan',
+    age: 29,
+    location: 'Gurgaon, India'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unabel to connect to the MongoDB server')
+    }
     console.log(JSON.stringify(result.ops, undefined, 2))
   })
-  
+
   db.close();
 
 })
