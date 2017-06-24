b079a3766c4a3cc8ce54d5813439662ea78171e4
Rohan Mazumdar <rohan1443@gmail.com>
2017-06-25 00:37:20 +0530

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -36,6 +36,19 @@ app.post('/users', (req, res) => {
   // })
 })
 
+//POST /users/login
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password'])
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+     user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
 //setting up a private route and middleware
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
